7cb9b0d34a9cd577f324b5959bf66f92d349138a
Rovanion Luckey <rovanion.luckey@gmail.com>
2011-10-23 03:59:23 -0700

    Initial commit

---
+++ bostad.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+curl -s http://www.graflunds.se/vara-lagenheter/lediga-laegenheter/linkoeping >> temp
+
+if cmp temp ingalagenheter ; then
+    echo "Inga nya lyor" > /dev/null
+else
+(
+    echo "From: BostadsBotten@rovanion.dyndns.org"
+    echo "Subject: Det finns bostad"
+    echo "Det finns bostad hos http://www.graflunds.se/vara-lagenheter/lediga-laegenheter/linkoeping"
+) | sendmail -t rovanion.luckey@gmail.com
+    
+    echo LÃ¤GENHET!
+fi
+
+rm temp
